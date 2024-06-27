aacdc3cf92f29f169f66855be0ac703c0bd8d2e3
SuperKojima <hayato.kojima@steamship.co.jp>
2024-06-27 11:06:40 +0900

    サーバー間オンリーで許される場合

--- app/frontend/app.vue
+++ app/frontend/app.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 // const data = await $fetch('http://api.kojima.localhost/sum?a=1&b=2')
-const { data } = useFetch('http://api.kojima.localhost/sum?a=1&b=2', {
-  lazy: false
+
+const params = reactive({
+  a: 1,
+  b: 2
 })
+
+const { data } = useFetch(`http://backend/sum?a=${params.a}&b=${params.b}`)
+// const { data } = useFetch(`http://backend/sum?a=${params.a}&b=${params.b}`)
+
+async function fire() {
+  const data = await $fetch(`http://backend/sum?a=${params.a}&b=${params.b}`)
+  console.log(data)
+}
 </script>
 
 <template>
@@ -11,5 +21,9 @@ const { data } = useFetch('http://api.kojima.localhost/sum?a=1&b=2', {
     <div>
       {{ data }}
     </div>
+    <input type="number" v-model.number="params.a" />
+    <input type="number" v-model.number="params.b" />
+
+    <button @click="fire">計算</button>
   </div>
 </template>
